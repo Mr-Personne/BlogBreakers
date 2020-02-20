541aadd0f813923562808fc12ad4a9e0c353761e
Mr-Personne <h.sacha07@gmail.com>
2020-02-20 13:38:22 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BlogBreakers</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <!-- section sacha -->
+
+    <!-- FIN section sacha -->
+
+
+    <!-- section fayçal -->
+
+    <!-- FIN section fayçal -->
+
+
+    <!-- section franck -->
+
+    <!-- FIN section franck -->
+
+
+    <!-- section glenn -->
+
+    <!-- FIN section glenn -->
+
+
+</body>
+</html>
