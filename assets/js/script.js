262b4a15011a3ae7f60bc189df4d5d6b3937162e
Mr-Personne <h.sacha07@gmail.com>
2020-02-20 13:53:52 +0000

    mise en place sections commentaires

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// section sacha
+
+// FIN section sacha
+
+
+// section fayçal
+
+// FIN section fayçal
+
+
+// section franck
+
+// FIN section franck
+
+
+// section glenn
+
+// FIN section glenn
---
+++ functions.php
@@ -0,0 +1,21 @@
+<?php
+// section sacha
+
+// FIN section sacha
+
+
+// section fayçal
+
+// FIN section fayçal
+
+
+// section franck
+
+// FIN section franck
+
+
+// section glenn
+
+// FIN section glenn
+
+?>
