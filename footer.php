4e95d3e6c72f7d6e88524e34728e000d04d35e74
Mr-Personne <h.sacha07@gmail.com>
2020-02-21 16:06:07 +0000

    ajout section widget

--- footer.php
+++ footer.php
@@ -1,6 +1,14 @@
         <footer>
             <h3>Ceci n'est pas un footer</h3>
             <p><?php wp_list_authors(); ?></p>
+
+            <!-- ajout de ma nouvelle widget area -->
+            <?php if (is_active_sidebar('new-widget-area')) : ?>
+                <div id="header-widget-area" class="nwa-header-widget widget-area" role="complementary">
+                    <?php dynamic_sidebar('new-widget-area'); ?>
+                </div>
+            <?php endif; ?>
+            <!-- fin nouvelle widget area -->
         </footer>
 
         <?php wp_footer(); ?>
