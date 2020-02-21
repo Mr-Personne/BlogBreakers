738b9ea63b18edb700e805fedcd280056fea664f
Mr-Personne <h.sacha07@gmail.com>
2020-02-21 10:32:52 +0000

    ajout fonction gestion des menus dans dashboard

--- functions.php
+++ functions.php
@@ -1,6 +1,12 @@
 <?php
 // section sacha
-
+function register_nisarg_menus() {
+    // This theme uses wp_nav_menu() in one location.
+    register_nav_menus( array(
+        'primary' => esc_html__( 'Top Primary Menu', 'nisarg' ),
+    ) );
+}
+add_action( 'init', 'register_nisarg_menus' );
 // FIN section sacha
 
 
