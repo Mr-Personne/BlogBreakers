d9ee2a90c323f82859f413f2ad376702c06c5e4f
Mr-Personne <h.sacha07@gmail.com>
2020-02-21 13:45:05 +0000

    ajout code page d'index avec h2 Specific page.php

--- page.php
+++ page.php
@@ -1,21 +1,50 @@
-<?php
-// section sacha
+<?php get_header(); ?>
 
-// FIN section sacha
+<main>
+    <!-- section sacha -->
+    <section>
+        <h2>This is a Page.php</h2>
+    </section>
+
+    <section>
+        
+       <?php
+        
+            if (have_posts()) :
+                /* Start the Loop */
+                while (have_posts()) : the_post()
+                    // echo 'post';
+                    
+        ?>
+                    <h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h2><small><?php the_time('F jS, Y') ?> by <?php the_author() ?> </small>
+        <?php
+                
+                endwhile;
+            else :
+
+                echo "<p>Sorry, there are no posts to display.</p>";
+
+            endif;
+        ?>
+        
+    </section>
+    <!-- FIN section sacha -->
 
 
-// section fayçal
+    <!-- section fayçal -->
 
-// FIN section fayçal
-
-
-// section franck
-
-// FIN section franck
+    <!-- FIN section fayçal -->
 
 
-// section glenn
+    <!-- section franck -->
 
-// FIN section glenn
+    <!-- FIN section franck -->
 
-?>
+
+    <!-- section glenn -->
+
+    <!-- FIN section glenn -->
+
+</main>
+
+<?php get_footer(); ?>
