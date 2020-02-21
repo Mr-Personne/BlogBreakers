dc37b73833cbba2a079a11e868b4e1462503c0df
Mr-Personne <h.sacha07@gmail.com>
2020-02-21 10:33:14 +0000

    ajout divers bloginfo()

--- header.php
+++ header.php
@@ -4,13 +4,29 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BlogBreakers</title>
-    <link rel="stylesheet" href="style.css">
+    <meta name="description" content="<?php bloginfo('description'); ?>">
+    <title><?php bloginfo('name'); ?> <?php wp_title('-'); ?></title>
+
+    <!-- bloginfo() info : https://developer.wordpress.org/reference/functions/bloginfo/ -->
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
     <?php wp_head(); ?>
 </head>
+
 
 <body <?php body_class(); ?>>
     <?php wp_body_open(); ?>
     <header>
-        <h1>BlogTest</h1>
+        <h1><?php bloginfo('name'); ?></h1>
+        <nav>
+            <!-- .navbar-header -->
+			<?php if ( has_nav_menu( 'primary' ) ) {
+				wp_nav_menu( array(
+					'theme_location'    => 'primary',
+					'container'         => 'div',
+					'container_class'   => 'collapse navbar-collapse navbar-ex1-collapse',
+					'menu_class'        => 'primary-menu',
+                ) ); } 
+                
+            ?>
+        </nav>
     </header>
