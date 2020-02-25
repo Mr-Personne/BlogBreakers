7732f3acb4eede09181f9c082a1d67f6a7bfdc82
Mr-Personne <h.sacha07@gmail.com>
2020-02-25 12:58:00 +0000

    test 3 widgets

--- footer.php
+++ footer.php
@@ -2,15 +2,29 @@
             <h3>Ceci n'est pas un footer</h3>
             <p><?php wp_list_authors(); ?></p>
 
-            <!-- ajout de ma nouvelle widget area -->
-            <?php if (is_active_sidebar('new-widget-area')) : ?>
-                <div id="header-widget-area" class="nwa-header-widget widget-area" role="complementary">
-                    <?php dynamic_sidebar('new-widget-area'); ?>
-                </div>
-            <?php endif; ?>
-            <!-- fin nouvelle widget area -->
+            <div style="display:flex; margin-right:50px;">
+                <!-- ajout de ma nouvelle widget area -->
+                <?php if (is_active_sidebar('widget-area-1')) : ?>
+                    <div style="margin-right:50px;">
+                        <?php dynamic_sidebar('widget-area-1'); ?>
+                    </div>
+                <?php endif; ?>
+                <?php if (is_active_sidebar('widget-area-2')) : ?>
+                    <div style="margin-right:50px;">
+                        <?php dynamic_sidebar('widget-area-2'); ?>
+                    </div>
+                <?php endif; ?>
+                <?php if (is_active_sidebar('widget-area-3')) : ?>
+                    <div style="margin-right:50px;">
+                        <?php dynamic_sidebar('widget-area-3'); ?>
+                    </div>
+                <?php endif; ?>
+                <!-- fin nouvelle widget area -->
+
+            </div>
         </footer>
 
         <?php wp_footer(); ?>
-    </body>
-</html>
+        </body>
+
+        </html>
