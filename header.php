65d63f4a89bdecb8149aeedd032ff2e679d509f1
Mr-Personne <h.sacha07@gmail.com>
2020-02-28 12:40:02 +0000

    test styling header bootstrap

--- header.php
+++ header.php
@@ -66,12 +66,12 @@
 
             <nav class="navbar navbar-expand-md navbar-light bg-light" role="navigation">
                 <div class="container">
-                    <?php echo the_custom_logo(); ?>
+                    
                     <!-- Brand and toggle get grouped for better mobile display -->
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
-                    <a class="navbar-brand" href="#">Navbar</a>
+                    <a class="navbar-brand" href="#"><?php echo the_custom_logo(); ?></a>
                     <?php
                     wp_nav_menu(array(
                         'theme_location'    => 'primary',
@@ -79,7 +79,7 @@
                         'container'         => 'div',
                         'container_class'   => 'collapse navbar-collapse',
                         'container_id'      => 'bs-example-navbar-collapse-1',
-                        'menu_class'        => 'nav navbar-nav',
+                        'menu_class'        => 'nav navbar-nav wp-nav-menu justify-content-between',
                         'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
                         'walker'            => new WP_Bootstrap_Navwalker(),
                     ));
