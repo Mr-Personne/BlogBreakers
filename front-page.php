37193fbc0d398b1077d6deaa6c104d5a087803c8
Mr-Personne <h.sacha07@gmail.com>
2020-03-02 14:39:47 +0000

    suppr bouton test modal vers footer

--- front-page.php
+++ front-page.php
@@ -322,57 +322,7 @@
 
 
     <!-- section franck -->
-    <!-- Button trigger modal -->
-    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-        Launch demo modal
-    </button>
-
-    <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <!-- partie presentation -->
-
-                    <div class="container fluid">
-                        <div class="container">
-                            <div class="row">
-                                <div class="col-4 bg-gradiant2 text-white">
-                                    <h1 class="font-bernadette font-titre-avatar text-center  pb-3 pt-3">Franck</h1>
-                                    <h4 class="font-montserrat text-center pb-3 ">Developpeur Web Front End</h4>
-                                    <p class="font-montserrat pr-1">J'ai intégré la formation de designer web a
-                                        l'acs de vesoul afin de devenir developpeur web.</p>
-                                </div>
-                                <div class="col-8">
-                                    <div class="row justify-content-center">
-                                        <div class="col-6 d-flex justify-content-center"><img src="./assets/images/franck.png" alt=""></div>
-                                        <div class="col-6 bg-gradiant">
-                                            <h4 class="text-center text-white pb-3 pt-3">Social Media</h4>
-                                            <div class="row flex-column align-items-center">
-                                                <a class="pb-4" href="https://www.facebook.com/AccessCodeSchool/?ref=br_rs" target="_blank"><img class="social-media" src="./assets/images/facebook-logo.png" alt="logo-facebook"></a>
-                                                <a class="pb-4" href="https://www.linkedin.com/mynetwork/" target="_blank"><img class="social-media" src="./assets/images/linkedin-logo.png" alt="logo-linkedin"></a>
-                                                <a class="pb-4" href="https://github.com/franck242" target="_blank"><img class="social-media" src="./assets/images/github-logo.png" alt="logo-github"></a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <!-- <button type="button" class="btn btn-primary">Save changes</button> -->
-                </div>
-            </div>
-        </div>
-    </div>
+    
     <!-- FIN section franck -->
 
 
