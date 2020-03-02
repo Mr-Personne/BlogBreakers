2d7d2475afbfb65e53516a099c3698949c0753af
Mr-Personne <h.sacha07@gmail.com>
2020-03-02 12:19:26 +0000

    teste pour page présentation

---
+++ equipier.php
@@ -0,0 +1,51 @@
+<?php /* Template Name: PageEquiper */ ?>
+<?php get_header(); ?>
+
+<main>
+    <!-- section sacha -->
+    
+    <!-- FIN section sacha -->
+
+
+    <!-- section fayçal -->
+
+    <!-- FIN section fayçal -->
+
+
+    <!-- section franck -->
+    <!-- partie presentation -->
+
+    <div class="container fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col-4 bg-gradiant2 text-white">
+                    <h1 class="font-bernadette font-titre-avatar text-center  pb-3 pt-3">Franck</h1>
+                    <h4 class="font-montserrat text-center pb-3 ">Developpeur Web Front End</h4>
+                    <p class="font-montserrat pr-1">J'ai intégré la formation de designer web a l'acs de vesoul afin de devenir developpeur web.</p>
+                </div>
+                <div class="col-8">
+                    <div class="row justify-content-center">
+                        <div class="col-6 d-flex justify-content-center"><img src="./assets/images/franck.png" alt=""></div>
+                        <div class="col-6 bg-gradiant">
+                            <h4 class="text-center text-white pb-3 pt-3">Social Media</h4>
+                            <div class="row flex-column align-items-center">
+                                <a class="pb-4" href="https://www.facebook.com/AccessCodeSchool/?ref=br_rs" target="_blank"><img class="social-media" src="./assets/images/facebook-logo.png" alt="logo-facebook"></a>
+                                <a class="pb-4" href="https://www.linkedin.com/mynetwork/" target="_blank"><img class="social-media" src="./assets/images/linkedin-logo.png" alt="logo-linkedin"></a>
+                                <a class="pb-4" href="https://github.com/franck242" target="_blank"><img class="social-media" src="./assets/images/github-logo.png" alt="logo-github"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- FIN section franck -->
+
+
+    <!-- section glenn -->
+
+    <!-- FIN section glenn -->
+
+</main>
+
+<?php get_footer(); ?>
